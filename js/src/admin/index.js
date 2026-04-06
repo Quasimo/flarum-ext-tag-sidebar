92d64bcfe981980b4aa1d4b9489d56c4655f70fe
quasimo <quasimo@qq.com>
2026-04-06 16:01:57 +0800

    fix: check flarum.reg.get to distinguish 1.x from 2.x

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,8 +2,8 @@ import app from 'flarum/admin/app';
 
 export { default as extend } from './extend';
 
-// Flarum 1.x: flarum.reg does not exist, use app.extensionData
-if (typeof flarum === 'undefined' || !flarum.reg) {
+// Flarum 1.x: flarum.reg.get is not a function, use app.extensionData
+if (typeof flarum === 'undefined' || typeof flarum.reg === 'undefined' || typeof flarum.reg.get !== 'function') {
   app.initializers.add('quasimo-tag-sidebar', () => {
     app.extensionData
       .for('quasimo-tag-sidebar')
