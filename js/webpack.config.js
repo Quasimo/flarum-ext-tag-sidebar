4c52273a6ceb529c983166e5231c712593879d17
quasimo <quasimo@qq.com>
2026-04-06 16:15:00 +0800

    fix: remove flarum-webpack-config plugins that inject flarum.reg code, use plain webpack config

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,31 +1,47 @@
-const config = require('flarum-webpack-config');
-const base = config();
+const { NormalModuleReplacementPlugin } = require('webpack');
 
-// Override externals to support both Flarum 1.x (flarum.core.compat) and 2.x (flarum.reg.get)
-base.externals = [
-  { jquery: 'jQuery' },
-  function ({ request }, callback) {
-    let namespace;
-    let id;
-    let matches;
-
-    if ((matches = /^flarum\/(.+)$/.exec(request))) {
-      namespace = 'core';
-      id = matches[1];
-    } else if ((matches = /^ext:([^\/]+)\/(?:flarum-(?:ext-)?)?([^\/]+)(?:\/(.+))?$/.exec(request))) {
-      namespace = `${matches[1]}-${matches[2]}`;
-      id = matches[3];
-    } else {
-      return callback();
-    }
-
-    // Compatible with both 1.x (flarum.core.compat) and 2.x (flarum.reg.get)
-    const code = namespace === 'core'
-      ? `(typeof flarum.reg !== 'undefined' && typeof flarum.reg.get === 'function' ? flarum.reg.get('core', '${id}') : flarum.core.compat['${id}'])`
-      : `(typeof flarum.reg !== 'undefined' && typeof flarum.reg.get === 'function' ? flarum.reg.get('${namespace}', '${id}') : (flarum.extensions['${namespace}'] && flarum.extensions['${namespace}'].compat && flarum.extensions['${namespace}'].compat['${id}']))`;
-
-    return callback(null, `root ${code}`);
+module.exports = {
+  entry: {
+    forum: './forum.js',
+    admin: './admin.js',
   },
-];
 
-module.exports = base;
+  plugins: [
+    new NormalModuleReplacementPlugin(/^@babel\/runtime(.*)/, (resource) => {
+      const path = resource.request.split('@babel/runtime')[1];
+      resource.request = require.resolve(`@babel/runtime${path}`);
+    }),
+  ],
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+
+  externals: [
+    { jquery: 'jQuery' },
+    function ({ request }, callback) {
+      let namespace;
+      let id;
+      let matches;
+
+      if ((matches = /^flarum\/(.+)$/.exec(request))) {
+        namespace = 'core';
+        id = matches[1];
+      } else if ((matches = /^ext:([^\/]+)\/(?:flarum-(?:ext-)?)?([^\/]+)(?:\/(.+))?$/.exec(request))) {
+        namespace = `${matches[1]}-${matches[2]}`;
+        id = matches[3];
+      } else {
+        return callback();
+      }
+
+      // Support both Flarum 1.x (flarum.core.compat) and 2.x (flarum.reg.get)
+      const code = namespace === 'core'
+        ? `(typeof flarum.reg !== 'undefined' && typeof flarum.reg.get === 'function' ? flarum.reg.get('core', '${id}') : flarum.core.compat['${id}'])`
+        : `(typeof flarum.reg !== 'undefined' && typeof flarum.reg.get === 'function' ? flarum.reg.get('${namespace}', '${id}') : (flarum.extensions['${namespace}'] && flarum.extensions['${namespace}'].compat && flarum.extensions['${namespace}'].compat['${id}']))`;
+
+      return callback(null, `root ${code}`);
+    },
+  ],
+
+  devtool: 'source-map',
+};
