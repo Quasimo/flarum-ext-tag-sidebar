f1ef7d7b7db08b2f565c1fa8e7bb589248600215
quasimo <quasimo@qq.com>
2026-04-06 16:08:36 +0800

    fix: custom webpack externals to support both Flarum 1.x and 2.x module resolution

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,3 +1,31 @@
 const config = require('flarum-webpack-config');
+const base = config();
 
-module.exports = config();
+// Override externals to support both Flarum 1.x (flarum.core.compat) and 2.x (flarum.reg.get)
+base.externals = [
+  { jquery: 'jQuery' },
+  function ({ request }, callback) {
+    let namespace;
+    let id;
+    let matches;
+
+    if ((matches = /^flarum\/(.+)$/.exec(request))) {
+      namespace = 'core';
+      id = matches[1];
+    } else if ((matches = /^ext:([^\/]+)\/(?:flarum-(?:ext-)?)?([^\/]+)(?:\/(.+))?$/.exec(request))) {
+      namespace = `${matches[1]}-${matches[2]}`;
+      id = matches[3];
+    } else {
+      return callback();
+    }
+
+    // Compatible with both 1.x (flarum.core.compat) and 2.x (flarum.reg.get)
+    const code = namespace === 'core'
+      ? `(typeof flarum.reg !== 'undefined' && typeof flarum.reg.get === 'function' ? flarum.reg.get('core', '${id}') : flarum.core.compat['${id}'])`
+      : `(typeof flarum.reg !== 'undefined' && typeof flarum.reg.get === 'function' ? flarum.reg.get('${namespace}', '${id}') : (flarum.extensions['${namespace}'] && flarum.extensions['${namespace}'].compat && flarum.extensions['${namespace}'].compat['${id}']))`;
+
+    return callback(null, `root ${code}`);
+  },
+];
+
+module.exports = base;
